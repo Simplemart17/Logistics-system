209e3f8c720130eae55fe3881bc5a2175c95346f
Martins Aloba <aloba.martins@gmail.com>
2019-11-01 22:17:21 +0100

    add dependencies for project

---
+++ src/pages/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Landing = () => (
+  <div className="landing">
+    <div className="landing__container">
+      <div className="landing__container--jumbotron">
+        <h1>Get Your Parcels Delivered Anywhere With Our Services</h1>
+        <div className="register-btn-container">
+          <Link to="/register"><button className="register-button">Get Started</button></Link>
+        </div>
+      </div>
+    </div>
+    <div className="landing__illustration">
+      <img src="https://res.cloudinary.com/dq7p8ff2f/image/upload/v1572634306/Assets/logistics-illustration.png" alt="page illustration" />
+    </div>
+  </div>
+);
+
+export default Landing;
